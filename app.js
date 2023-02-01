7f6b48594e7fc59b91b3cfef0e88c3c378f3b7bf
Ilknur Tuncer <ilknurtuncer67@gmail.com>
2023-02-01 13:47:25 +0300

    apı remove

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const form = document.querySelector("section.top-banner form")
 const input = document.querySelector(".container input")
 const msg = document.querySelector("span.msg")
 const list = document.querySelector(".ajax-section ul.cities")
-// localStorage.setItem("tokenKeyEncrypted", EncryptStringAES("a81e00cee88a20b37ccecaa137895101"))
+
 
 localStorage.setItem("tokenKey", "fDtZU2IZlGgyfJpODqOw6CrTLR05jpZJTJi/5iJUxnSzwvzRsa7TnCkgerP75szJ")
 
